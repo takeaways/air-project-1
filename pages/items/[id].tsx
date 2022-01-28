917d8da6dc918919ef6e493ec2fb51eadc9d7ba0
takeaways <wkdrjsdlf2@gmail.com>
2022-01-29 06:44:13 +0900

    style: fix style

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -6,7 +6,7 @@ const ItemDetail: NextPage = () => {
       <div className="mb-8">
         <div className="h-96 bg-slate-300" />
         <div className="flex items-center py-3 space-x-3 border-t border-b cursor-pointer">
-          <div className="w-12 h-12 rounded-full bg-slate-300 " />
+          <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
             <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
             <p className="text-xs font-medium text-gray-500">
@@ -15,9 +15,9 @@ const ItemDetail: NextPage = () => {
           </div>
         </div>
         <div className="mt-5">
-          <h1 className="text-3xl font-extrabold text-gray-900">Galaxy S50</h1>
-          <p className="block mt-3 text-3xl text-gray-900">$140</p>
-          <p className="my-6 text-base text-gray-700">
+          <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
+          <span className="block mt-3 text-3xl text-gray-900">$140</span>
+          <p className="my-6 text-gray-700 ">
             My money&apos;s in that office, right? If she start giving me some
             bullshit about it ain&apos;t there, and we got to go someplace else
             and get it, I&apos;m gonna shoot you in the head then and there.
@@ -27,11 +27,11 @@ const ItemDetail: NextPage = () => {
             and that ni**a Winston or anybody else is in there, you the first
             motherfucker to get shot. You understand?
           </p>
-          <div className="flex items-center justify-between">
-            <button className="flex-1 py-3 font-medium text-white bg-orange-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-orange-600 focus:ring-orange-500">
+          <div className="flex items-center justify-between space-x-2">
+            <button className="flex-1 py-3 font-medium text-white bg-orange-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-orange-600 focus:ring-orange-500 ">
               Talk to seller
             </button>
-            <button className="flex items-center justify-center p-3 text-gray-400 rounded-md hover:text-gray-700">
+            <button className="flex items-center justify-center p-3 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500">
               <svg
                 className="w-6 h-6 "
                 xmlns="http://www.w3.org/2000/svg"
@@ -52,13 +52,13 @@ const ItemDetail: NextPage = () => {
         </div>
       </div>
       <div>
-        <h2 className="text-2x, font-extrabold text-gray-700">Similar items</h2>
-        <div className="grid grid-cols-2 gap-4">
+        <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
+        <div className="grid grid-cols-2 gap-4 mt-6 ">
           {[1, 2, 3, 4, 5, 6].map((_, i) => (
             <div key={i}>
-              <div className="w-full h-56 mt-6 mb-4 bg-slate-300" />
-              <h3 className="-mb-1 gray-700">Galaxy S60</h3>
-              <p className="text-sm font-medium text-gray-900">$6</p>
+              <div className="w-full h-56 mb-4 bg-slate-300" />
+              <h3 className="-mb-1 text-gray-700">Galaxy S60</h3>
+              <span className="text-sm font-medium text-gray-900">$6</span>
             </div>
           ))}
         </div>
