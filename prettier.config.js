5305c6738c4533dc4b6d396cc3a7de9fff2b6771
calix_air <calix_air@calix-airui-MacBookAir.local>
2022-01-29 01:36:35 +0900

    init 프로젝트 셋팅

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  importOrder: [
+    '^@core/(.*)$',
+    '^pages/(.*)$',
+    '^components/(.*)$',
+    '^hooks/(.*)$',
+    '^types/(.*)$',
+    '^utils/(.*)$',
+    '^@server/(.*)$',
+    '^@ui/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
