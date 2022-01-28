8099227d237cf6430940e1146299f8adb0068e7e
takeaways <wkdrjsdlf2@gmail.com>
2022-01-29 06:44:35 +0900

    feat: 상세페이지 스타일

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,48 +2,55 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div>
+    <div className="px-4 py-16">
       <div>
-        <div>
-          <label>
-            <svg
-              className="w-12 h-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-
-            <input type="file" />
-          </label>
-        </div>
+        <label className="flex items-center justify-center w-full h-48 py-6 text-gray-600 border-2 border-gray-300 border-dashed rounded-md cursor-pointer hover:border-orange-500 hover:text-orange-500">
+          <svg
+            className="w-12 h-12"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <input type="file" className="hidden" />
+        </label>
       </div>
-      <div>
-        <label>Price</label>
-        <div>
-          <div>
-            <span>$</span>
+      <div className="my-5">
+        <label htmlFor="price" className="text-sm font-medium text-gray-700">
+          Price
+        </label>
+        <div className="relative flex items-center rounded-md shadow-sm ">
+          <div className="absolute left-0 flex items-center justify-center pl-3">
+            <span className="text-sm text-gray-500 pointer-events-none">$</span>
           </div>
-          <input type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
+          <input
+            id="price"
+            type="text"
+            placeholder="0.00"
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500 pl-7"
+          />
+          <div className="absolute right-0 flex items-center pr-3 pointer-events-none ">
+            <span className="text-gray-500">USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label>Description</label>
-        <div>
-          <textarea rows={4} />
-        </div>
+        <label className="text-sm font-medium text-gray-700">Description</label>
+        <textarea
+          rows={4}
+          className="w-full mt-1 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500"
+        />
       </div>
-      <button>Upload product</button>
+      <button className="w-full px-4 py-2 mt-2 text-sm font-medium text-white bg-orange-500 border border-transparent rounded-md shadow-sm hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Upload product
+      </button>
     </div>
   );
 };
