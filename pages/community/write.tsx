b4a4812819ab87fa88910becd7882c0e1ba8cb72
takeaways <wkdrjsdlf2@gmail.com>
2022-01-29 22:56:35 +0900

    마이그레이션

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from "next";
 
+import Button from "components/button";
+import Layout from "components/layout";
+import TextArea from "components/textarea";
+
 const Write: NextPage = () => {
   return (
-    <form className="px-4 py-10">
-      <textarea
-        className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500 "
-        rows={4}
-        placeholder="Ask a question!"
-      />
-      <button className="w-full px-4 py-2 mt-2 text-sm font-medium text-white bg-orange-500 border border-transparent rounded-md shadow-sm hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-        Submit
-      </button>
-    </form>
+    <Layout canGoBack title="Write Post">
+      <form className="p-4 space-y-4">
+        <TextArea required placeholder="Ask a question!" />
+        <Button text="Submit" />
+      </form>
+    </Layout>
   );
 };
 
--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -1,83 +1,46 @@
 import type { NextPage } from "next";
 
+import Button from "components/button";
+import Input from "components/input";
+import Layout from "components/layout";
+import TextArea from "components/textarea";
+
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-10 space-y-5">
-      <div>
-        <label className="flex items-center justify-center w-full h-48 text-gray-600 border-2 border-gray-300 border-dashed rounded-md cursor-pointer hover:border-orange-500 hover:text-orange-500">
-          <svg
-            className="w-12 h-12"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <input className="hidden" type="file" />
-        </label>
-      </div>
-      <div>
-        <label
-          className="block mb-1 text-sm font-medium text-gray-700"
-          htmlFor="name"
-        >
-          Name
-        </label>
-        <div className="relative flex items-center rounded-md shadow-sm">
-          <input
-            id="name"
-            type="email"
-            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            required
-          />
+    <Layout canGoBack title="Upload Product">
+      <form className="p-4 space-y-4">
+        <div>
+          <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
+            <svg
+              className="h-12 w-12"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input className="hidden" type="file" />
+          </label>
         </div>
-      </div>
-      <div>
-        <label
-          className="block mb-1 text-sm font-medium text-gray-700"
-          htmlFor="price"
-        >
-          Price
-        </label>
-        <div className="relative flex items-center rounded-md shadow-sm">
-          <div className="absolute left-0 flex items-center justify-center pl-3 pointer-events-none">
-            <span className="text-sm text-gray-500">$</span>
-          </div>
-          <input
-            id="price"
-            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none pl-7 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            type="text"
-            placeholder="0.00"
-          />
-          <div className="absolute right-0 flex items-center pr-3 pointer-events-none">
-            <span className="text-gray-500">USD</span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <label
-          htmlFor="description"
-          className="block mb-1 text-sm font-medium text-gray-700"
-        >
-          Description
-        </label>
-
-        <textarea
-          id="description"
-          className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500 "
-          rows={4}
+        <Input required label="Name" name="name" type="text" />
+        <Input
+          required
+          label="Price"
+          placeholder="0.00"
+          name="price"
+          type="text"
+          kind="price"
         />
-      </div>
-      <button className="w-full px-4 py-2 text-sm font-medium text-white bg-orange-500 border border-transparent rounded-md shadow-sm  hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload item
-      </button>
-    </div>
+        <TextArea name="description" label="Description" />
+        <Button text="Upload item" />
+      </form>
+    </Layout>
   );
 };
 
