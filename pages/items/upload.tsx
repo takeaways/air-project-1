ac71515cd64e10c8f0713f123b5e7530dc5aa2f1
takeaways <wkdrjsdlf2@gmail.com>
2022-01-29 21:52:55 +0900

    feat: items upload name 필드 추가

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-10 space-y-5">
       <div>
-        <label className="flex items-center justify-center w-full h-48 py-6 text-gray-600 border-2 border-gray-300 border-dashed rounded-md cursor-pointer hover:border-orange-500 hover:text-orange-500">
+        <label className="flex items-center justify-center w-full h-48 text-gray-600 border-2 border-gray-300 border-dashed rounded-md cursor-pointer hover:border-orange-500 hover:text-orange-500">
           <svg
             className="w-12 h-12"
             stroke="currentColor"
@@ -19,37 +19,63 @@ const Upload: NextPage = () => {
               strokeLinejoin="round"
             />
           </svg>
-          <input type="file" className="hidden" />
+          <input className="hidden" type="file" />
         </label>
       </div>
-      <div className="my-5">
-        <label htmlFor="price" className="text-sm font-medium text-gray-700">
+      <div>
+        <label
+          className="block mb-1 text-sm font-medium text-gray-700"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="relative flex items-center rounded-md shadow-sm">
+          <input
+            id="name"
+            type="email"
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            required
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          className="block mb-1 text-sm font-medium text-gray-700"
+          htmlFor="price"
+        >
           Price
         </label>
-        <div className="relative flex items-center rounded-md shadow-sm ">
-          <div className="absolute left-0 flex items-center justify-center pl-3">
-            <span className="text-sm text-gray-500 pointer-events-none">$</span>
+        <div className="relative flex items-center rounded-md shadow-sm">
+          <div className="absolute left-0 flex items-center justify-center pl-3 pointer-events-none">
+            <span className="text-sm text-gray-500">$</span>
           </div>
           <input
             id="price"
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none pl-7 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
             type="text"
             placeholder="0.00"
-            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500 pl-7"
           />
-          <div className="absolute right-0 flex items-center pr-3 pointer-events-none ">
+          <div className="absolute right-0 flex items-center pr-3 pointer-events-none">
             <span className="text-gray-500">USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label className="text-sm font-medium text-gray-700">Description</label>
+        <label
+          htmlFor="description"
+          className="block mb-1 text-sm font-medium text-gray-700"
+        >
+          Description
+        </label>
+
         <textarea
+          id="description"
+          className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500 "
           rows={4}
-          className="w-full mt-1 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500"
         />
       </div>
-      <button className="w-full px-4 py-2 mt-2 text-sm font-medium text-white bg-orange-500 border border-transparent rounded-md shadow-sm hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product
+      <button className="w-full px-4 py-2 text-sm font-medium text-white bg-orange-500 border border-transparent rounded-md shadow-sm  hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Upload item
       </button>
     </div>
   );
