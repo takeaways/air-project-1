d8864376a602ff5b1e97fbcd7e8ab7e339355c99
takeaways <wkdrjsdlf2@gmail.com>
2022-01-30 21:33:17 +0900

    feat: user api

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,9 +2,39 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import client from "libs/server/client";
 import withHandler from "libs/server/withHandler";
+import { ResponseType } from "libs/server/withHandler";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json({ ok: true });
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
+  const {
+    body: { phone, email },
+  } = req;
+
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) {
+    return res.status(400).json({ ok: false });
+  }
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymouse",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+
+  res.status(201).json({ ok: true });
 }
 
 export default withHandler("POST", handler);
