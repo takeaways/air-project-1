a865c700b2c682fb9fcb818e1d1a28a7cdad05c9
takeaways <wkdrjsdlf2@gmail.com>
2022-01-31 00:38:26 +0900

    feat: send SMS and Email for Confirm

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -9,8 +9,8 @@ declare module "iron-session" {
 }
 
 const cookieOptions = {
-  cookieName: "carrotsession",
-  password: process.env.COOKIE_PASSWORD!,
+  cookieName: "geonilairsession",
+  password: "testcoooooo_)(_)@(%_)(#%_93-0529-359-203-0239-09-0okiee",
 };
 
 export function withApiSession(fn: any) {
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,3 +1,4 @@
+import main from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
 
@@ -6,6 +7,7 @@ import withHandler from "libs/server/withHandler";
 import { ResponseType } from "libs/server/withHandler";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+main.setApiKey(process.env.SENDGRID_API_KEY!);
 
 async function handler(
   req: NextApiRequest,
@@ -38,11 +40,19 @@ async function handler(
   });
 
   if (phone) {
-    await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}`,
-    });
+    // await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MY_PHONE!,
+    //   body: `Your login token is ${payload}`,
+    // });
+  } else if (email) {
+    // const email = await main.send({
+    //   from: "jgi0105@naver.com",
+    //   to: "jgi0105@naver.com",
+    //   subject: "geonil jang",
+    //   text: `Your token is ${payload}`,
+    //   html: `<strong>Your token is ${payload}</strong>`,
+    // });
   }
 
   res.status(201).json({ ok: true });
