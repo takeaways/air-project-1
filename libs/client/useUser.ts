4c319f37a59b0459820a667b316f74e273549843
takeaways <wkdrjsdlf2@gmail.com>
2022-02-05 23:41:22 +0900

    feat: way2 global config setup

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,12 +2,8 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import useSWR from "swr";
 
-const fetcher = (url: string) => {
-  return fetch(url).then((response) => response.json());
-};
-
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me", fetcher);
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
 
   useEffect(() => {
