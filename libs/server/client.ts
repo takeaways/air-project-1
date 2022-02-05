33549ccc6db64f3a0a4fa099b24b3beb55c04910
takeaways <wkdrjsdlf2@gmail.com>
2022-02-06 00:19:56 +0900

    feat: add Product Model

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
+const client = global.client || new PrismaClient();
+
+
+declare global{
+  var client: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
 export default new PrismaClient();
